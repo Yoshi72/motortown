e647debe56319dd66b99425f8c5368a6b443d6fd
Yoshi72 <shirakami72+github@gmail.com>
2026-05-12 19:16:55 +0200

    Update entry.sh

--- etc/entry.sh
+++ etc/entry.sh
@@ -120,7 +120,7 @@ source "${STEAMAPPDIR}/pre.sh"
 # Start Server
 
 echo "Starting MotorTown Dedicated Server - ${SERVER_HOSTNAME}"
-eval bash "${STEAMCMDDIR}/steamcmd.sh" "${STEAMCMD_SPEW}" +app_run "${STEAMAPPID}" -beta beta -betapassword motortowndedi
+eval bash "${STEAMAPPDIR}/RunDedicatedServer.bat"
 
 # Post Hook
 source "${STEAMAPPDIR}/post.sh"
